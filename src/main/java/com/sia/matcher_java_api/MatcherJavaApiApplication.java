007d42c03099d964b704f5ba7db290a99d15d45a
fortune00 <sinyoungbok99@gmail.com>
2022-07-04 18:39:41 +0900

    [chore] Springboot, Hibernate-Spatial, Postgresql 환경 설정

---
+++ src/main/java/com/sia/matcher_java_api/MatcherJavaApiApplication.java
@@ -0,0 +1,11 @@
+package com.sia.matcher_java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcherJavaApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MatcherJavaApiApplication.class, args);
+    }
+}
